949593cf2ac90e625de60db8ff1ca514d23a5b2d
Dominik Kruczek <TheKiromen@gmail.com>
2023-03-09 17:01:19 +0100

    Initial commit
    Initial file structure.
    Embedded iframe with p5.js sketch.

---
+++ src/Components/MainPage/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="https://editor.p5js.org/TheKiromen/full/lYCE4VGoi"></iframe>
+    </div>
+  );
+}
+
+export default App;
