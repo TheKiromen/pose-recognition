593b7fedf2aa00a3c4d7abcb177df7eb0097775a
Dominik Kruczek <TheKiromen@gmail.com>
2023-03-15 15:12:06 +0100

    Camera apsect ratio FIX

--- src/Components/MainPage/poseRecognition.js
+++ src/Components/MainPage/poseRecognition.js
@@ -6,19 +6,15 @@ function PoseRecognition() {
     let video;
 
     const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(500, 500).parent(canvasParentRef);
         video = p5.createCapture(p5.VIDEO);
+        p5.createCanvas(500, 500 * video.height / video.width).parent(canvasParentRef);
         video.hide();
     };
 
-    //TODO Check for camera's aspect reatio's
     const draw = (p5) => {
         // p5.translate(video.width, 0);
         // p5.scale(-1, 1);
         p5.image(video,0,0,p5.width, p5.width * video.height / video.width);
-        p5.stroke("red");
-        p5.strokeWeight(3);
-        p5.line(0, 281, p5.width, 281);
     };
 
     return <Sketch setup={setup} draw={draw} />;
