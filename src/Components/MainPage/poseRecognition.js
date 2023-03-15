957f99f6221ea744b5a80bee870f1df7558c4360
Dominik Kruczek <TheKiromen@gmail.com>
2023-03-15 17:09:40 +0100

    Pose detection
    Implemented clown detection using poseNet model.

--- src/Components/MainPage/poseRecognition.js
+++ src/Components/MainPage/poseRecognition.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Sketch from "react-p5";
+import * as ml5 from "ml5";
 
 function PoseRecognition() {
 
     let video;
+    let poseNet;
+    let currentPose;
 
     const setup = (p5, canvasParentRef) => {
         //Create canvas and set parent component
@@ -12,12 +15,17 @@ function PoseRecognition() {
         video = p5.createCapture(p5.VIDEO);
         //Hide webcam preview
         video.hide();
+
+        //Initialize poseNet;
+        poseNet = ml5.poseNet(video, () => console.log("PoseNet Ready!"));
+        //Setup callback on pose detection
+        poseNet.on('pose', poseDetected);
     };
 
     const draw = (p5) => {
         //OBS 1920 x 1080 (16:9)
         //CAM Mine 640 x 480 (4:3)
-        //CAN Pablo
+        //CAN Pablo 640 x 480 (4:3)
         //CAM Wojtke
         //TODO Test webcam aspect ratios
         // console.log(video.width + " " + video.height);
@@ -27,7 +35,20 @@ function PoseRecognition() {
         p5.translate(-640, 0);
         //Draw image from webcam
         p5.image(video, 0, 0, p5.width, p5.width * video.height / video.width);
+
+        if(currentPose){
+            p5.fill("red");
+            p5.ellipse(currentPose.nose.x, currentPose.nose.y, 50);
+        }
     };
+
+    function poseDetected(poses){
+        //If there is at least 1 pose detected
+        if(poses.length > 0){
+            currentPose = poses[0].pose;
+            console.log(currentPose);
+        }
+    }
 
     return <Sketch setup={setup} draw={draw} />;
 }
