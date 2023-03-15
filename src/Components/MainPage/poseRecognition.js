0703855c51f6b674e8148a9c1df8175ee9fb033d
Dominik Kruczek <TheKiromen@gmail.com>
2023-03-15 13:05:21 +0100

    Camera acess
    Implemented accessing camera feed and displlaying it on canvas.

--- src/Components/MainPage/poseRecognition.js
+++ src/Components/MainPage/poseRecognition.js
@@ -2,14 +2,20 @@ import React from "react";
 import Sketch from "react-p5";
 
 function PoseRecognition() {
+
+    let video;
+
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(500, 500).parent(canvasParentRef);
+        video = p5.createCapture(p5.VIDEO);
+        video.hide();
     };
 
+    //TODO Check for camera's aspect reatio's
     const draw = (p5) => {
-        p5.background(255, 130, 20)
-        p5.ellipse(100, 100, 100)
-        p5.ellipse(300, 100, 100)
+        // p5.translate(video.width, 0);
+        // p5.scale(-1, 1);
+        p5.image(video,0,0,p5.width, p5.width * video.height / video.width);
     };
 
     return <Sketch setup={setup} draw={draw} />;
