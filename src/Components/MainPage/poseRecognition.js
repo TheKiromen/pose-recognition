5e4afddea38638a4a422dad1bf83915b02c50e96
Dominik Kruczek <TheKiromen@gmail.com>
2023-03-15 16:03:36 +0100

    Canvas aspect ratio
    Changed canvas aspect ratio to match webcam's.

--- src/Components/MainPage/poseRecognition.js
+++ src/Components/MainPage/poseRecognition.js
@@ -6,15 +6,27 @@ function PoseRecognition() {
     let video;
 
     const setup = (p5, canvasParentRef) => {
+        //Create canvas and set parent component
+        p5.createCanvas(640, 480).parent(canvasParentRef);
+        //Get webcam data
         video = p5.createCapture(p5.VIDEO);
-        p5.createCanvas(500, 500 * video.height / video.width).parent(canvasParentRef);
+        //Hide webcam preview
         video.hide();
     };
 
     const draw = (p5) => {
-        // p5.translate(video.width, 0);
-        // p5.scale(-1, 1);
-        p5.image(video,0,0,p5.width, p5.width * video.height / video.width);
+        //OBS 1920 x 1080 (16:9)
+        //CAM Mine 640 x 480 (4:3)
+        //CAN Pablo
+        //CAM Wojtke
+        //TODO Test webcam aspect ratios
+        // console.log(video.width + " " + video.height);
+
+        //Flip image horizontally
+        p5.scale(-1, 1);
+        p5.translate(-640, 0);
+        //Draw image from webcam
+        p5.image(video, 0, 0, p5.width, p5.width * video.height / video.width);
     };
 
     return <Sketch setup={setup} draw={draw} />;
