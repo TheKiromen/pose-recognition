52fbed83675e4e3e989a00097527368a20aaba4c
TytusKolpak <101522301+TytusKolpak@users.noreply.github.com>
2023-03-16 19:42:21 +0100

    Logout put in place
    For now it is only a placeholder because the logging way will change.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,13 +8,18 @@ function MainNavbar() {
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="/">Pose recognition</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="login">Login</Nav.Link>
                         <Nav.Link href="controlPanel">Control Panel</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
+
+                {/* The way of being logged in will change so for now only a placeholder wil be put in place */}
+                <div id="logout">
+                    Logout
+                </div>
             </Container>
         </Navbar>
     );
