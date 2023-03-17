b00e3f8867e61db442b9087b9126c0b60181736c
TytusKolpak <101522301+TytusKolpak@users.noreply.github.com>
2023-03-17 19:41:45 +0100

    Control Panel styled
    Designed and created the control panel - first take.

--- src/Components/ControlPanel/controllPanel.js
+++ src/Components/ControlPanel/controllPanel.js
@@ -1,22 +1,64 @@
 import {useEffect, useState} from "react";
-import { Navigate } from "react-router-dom";
+import {Navigate} from "react-router-dom";
+import "./controllPanel.css"
 
 //FIXME Change to use useState webhook
 
 const Panel = () => {
     const test = localStorage.getItem("authenticated") === "true" ? true : false;
-    console.log(typeof(localStorage.getItem("authenticated")));
+    console.log(typeof (localStorage.getItem("authenticated")));
     if (test) {
         //Redirect
         console.log(localStorage.getItem("authenticated"));
         return (
             <div className="Panel">
-                CONTROL PANEL
+                <div id="menu">
+                    <div>
+                        {/* Empty div here to create a gap */}
+                    </div>
+                    <div>
+                        Użytkownik: xXKiller69Xx
+                    </div>
+                    <div>
+                        Lorem: Ipsum
+                    </div>
+                    <div>
+                        Pierogi: Ruskie
+                    </div>
+                    <div>
+                        Hotel: Trivago
+                    </div>
+                </div>
+                <div id="figures">
+                    {/*first row with 2 items inside*/}
+                    <div id="row_of_figures">
+                        <div>
+                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                            Figure A
+                        </div>
+                        <div>
+                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                            Figure B
+                        </div>
+                    </div>
+
+                    {/*second row with 2 items inside*/}
+                    <div id="row_of_figures">
+                        <div>
+                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                            Figure C
+                        </div>
+                        <div>
+                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                            Figure D
+                        </div>
+                    </div>
+                </div>
             </div>
         );
 
     } else {
-        return <Navigate replace to="/login" />;
+        return <Navigate replace to="/login"/>;
     }
 };
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,33 +7,19 @@ const CreatorsFooter = () => {
     return (
         <div id="centering_id">
             <div id="bottom_of_the_page">
-                By:
-                <div>
-                    <a href="https://github.com/TheKiromen">
-                        D. Kruczek
-                    </a>
+                <div id="autors_half">
+                    By:
+                    <div><a href="https://github.com/TheKiromen">D. Kruczek</a></div>
+
+                    <div><a href="https://github.com/pawelus70">P. Blak</a></div>
+
+                    <div><a href="https://github.com/Trodaire98">W. Badura</a></div>
+
+                    <div><a href="https://github.com/TytusKolpak">T. Kołpak</a></div>
                 </div>
 
-                <div>
-                    <a href="https://github.com/pawelus70">
-                        P. Blak
-                    </a>
-                </div>
-
-                <div>
-                    <a href="https://github.com/Trodaire98">
-                        W. Badura
-                    </a>
-                </div>
-
-                <div>
-                    <a href="https://github.com/TytusKolpak">
-                        T. Kołpak
-                    </a>
-                </div>
-
-                <div id="copyright">
-                    Copyright {date}©
+                <div id="half_of_footer">
+                    <div id="copyright">Copyright {date}©</div>
                 </div>
             </div>
         </div>
