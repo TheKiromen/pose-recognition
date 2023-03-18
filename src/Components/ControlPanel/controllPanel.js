519a5e526026e6133b04501fe76ca950c0ca3b6a
TytusKolpak <101522301+TytusKolpak@users.noreply.github.com>
2023-03-18 10:01:52 +0100

    Fixed control panel scroll behavior
    It used to appear too late - had to add 2 divs to enlarge it. Now there is 20px gap between footer and panel at all times.

--- src/Components/ControlPanel/controllPanel.js
+++ src/Components/ControlPanel/controllPanel.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import "./controllPanel.css"
-import { Navigate } from "react-router-dom";
-import { getAuth, onAuthStateChanged  } from "firebase/auth";
+import {Navigate} from "react-router-dom";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
 
 //FIXME Change to use useState webhook
 
@@ -12,46 +12,56 @@ const Panel = () => {
         //Redirect
         console.log(localStorage.getItem("authenticated"));
         return (
-            <div className="Panel">
-                <div id="menu">
-                    <div>
-                        {/* Empty div here to create a gap */}
-                    </div>
-                    <div>
-                        Użytkownik: xXKiller69Xx
-                    </div>
-                    <div>
-                        Lorem: Ipsum
-                    </div>
-                    <div>
-                        Pierogi: Ruskie
-                    </div>
-                    <div>
-                        Hotel: Trivago
-                    </div>
-                </div>
-                <div id="figures">
-                    {/*first row with 2 items inside*/}
-                    <div id="row_of_figures">
-                        <div>
-                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
-                            Figure A
+            // For the margin bottom to work there has to be a div with it and another
+            // div encapsulating div enlarged in this way. Both have to be flex
+            <div id="encapsulating">
+                <div id="enlarged">
+                    <div className="Panel">
+                        <div id="menu">
+                            <div>
+                                {/* Empty div here to create a gap */}
+                            </div>
+                            <div>
+                                Użytkownik: xXKiller69Xx
+                            </div>
+                            <div>
+                                Lorem: Ipsum
+                            </div>
+                            <div>
+                                Pierogi: Ruskie
+                            </div>
+                            <div>
+                                Hotel: Trivago
+                            </div>
                         </div>
-                        <div>
-                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
-                            Figure B
-                        </div>
-                    </div>
+                        <div id="figures">
+                            {/*first row with 2 items inside*/}
+                            <div id="row_of_figures">
+                                <div>
+                                    <img
+                                        src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                                    Figure A
+                                </div>
+                                <div>
+                                    <img
+                                        src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                                    Figure B
+                                </div>
+                            </div>
 
-                    {/*second row with 2 items inside*/}
-                    <div id="row_of_figures">
-                        <div>
-                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
-                            Figure C
-                        </div>
-                        <div>
-                            <img src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
-                            Figure D
+                            {/*second row with 2 items inside*/}
+                            <div id="row_of_figures">
+                                <div>
+                                    <img
+                                        src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                                    Figure C
+                                </div>
+                                <div>
+                                    <img
+                                        src={"https://www.researchgate.net/publication/337727529/figure/fig4/AS:941575693684771@1601500547534/Annual-rainfall-in-the-MDB-1900-2018-Source-BOM-Available-at.png"}/>
+                                    Figure D
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
