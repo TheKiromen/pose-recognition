272cc553d8b838c3a6d8fec25d73fd5e7481c8a3
Dominik Kruczek <TheKiromen@gmail.com>
2023-03-27 15:43:50 +0200

    Changed Init
    Moved model initialziation to main component useEffect

--- src/Components/MainPage/App.js
+++ src/Components/MainPage/App.js
@@ -5,6 +5,9 @@ import ControlPanel from "../ControlPanel/controllPanel";
 import Logout from "../LogoutPage/LogoutForm";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {ModelContext} from "../Context/ModelContext";
+import {useEffect, useState} from "react";
+import * as processing from 'p5'
+import * as ml5 from 'ml5'
 
 // Elementy odpowiedzialne za Bootstrapa
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,13 +15,47 @@ import MainNavbar from "../Navbar";
 import CreatorsFooter from "../Footer"
 import ProtectedRouted from "../RouteSettings/ProtectedRouted";
 import SessionRoute from "../RouteSettings/SessionRoute";
-import {useEffect, useState} from "react";
+
 
 function App() {
     const [test, setTest] = useState("Initial Value");
+	const [models, setModels] = useState(null);
+
+	let video;
+	let poseNet;
+	let model;
+	let p5;
+
+	const options = {
+		inputs: 34,
+		outputs: 1,
+		task: 'classification',
+		debug: true
+	}
+	const modelInfo = {
+		model: 'ModelData/model.json',
+		metadata: 'ModelData/model_meta.json',
+		weights: 'ModelData/model.weights.bin',
+	};
 
 	useEffect(()=>{
-		console.log("Model init here!")
+		//Initialize p5.js
+		p5 = new processing();
+
+		//Initialize model
+		model = ml5.neuralNetwork(options);
+
+		//Load trained model data
+		model.load(modelInfo, () => {
+			console.log("Main Model loaded!");
+		});
+
+		//Initialize capture
+		video = p5.createCapture(p5.VIDEO);
+
+		//Initialize poseNet
+		poseNet = ml5.poseNet(video, () => console.log("Main PoseNet Ready!"));
+
 	},[])
 
     return (
